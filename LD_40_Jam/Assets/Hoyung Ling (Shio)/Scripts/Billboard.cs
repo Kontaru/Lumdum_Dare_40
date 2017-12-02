ed3731e9e53c0cd84b370941f73fb279cac2d657
Kontaru <drwafflesxd@gmail.com>
2017-12-02 13:09:29 +0000

    Hoyung: Enemy Sprites Done

--- LD_40_Jam/Assets/Hoyung Ling (Shio)/Scripts/Billboard.cs
+++ LD_40_Jam/Assets/Hoyung Ling (Shio)/Scripts/Billboard.cs
@@ -6,11 +6,11 @@ public class Billboard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        transform.LookAt(Camera.main.transform);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        transform.LookAt(Camera.main.transform);
+    }
 }
