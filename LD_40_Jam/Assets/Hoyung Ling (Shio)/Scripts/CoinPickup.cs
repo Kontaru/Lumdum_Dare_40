a468a33851f6a912337915a6e614b4dacaba6c2a
Kontaru <drwafflesxd@gmail.com>
2017-12-02 23:55:51 +0000

    Hoyung: Coin graphic, coin animation

---
+++ LD_40_Jam/Assets/Hoyung Ling (Shio)/Scripts/CoinPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+	void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            coll.gameObject.SendMessage("TakeCoin", 1, SendMessageOptions.DontRequireReceiver);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ LD_40_Jam/Assets/Hoyung Ling (Shio)/Scripts/PC/PC_Pouch.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PC_Pouch : MonoBehaviour {
+
+    //Coin animation
+    public Image coinGraphic;
+    float timeOfTravel = 1; //time after object reach a target place 
+    float currentTime = 0; // actual floting time 
+    float timeinterval = 0;
+    bool BL_Animate;
+    bool BL_Increase;
+
+    public Vector3 startPos;
+    public Vector3 endPos;
+
+    RectTransform myCoin = new RectTransform();
+    public int coins;
+
+	// Use this for initialization
+	void Start () {
+        myCoin = coinGraphic.GetComponent<RectTransform>();
+        startPos = myCoin.anchoredPosition;
+        endPos = myCoin.anchoredPosition + new Vector2(0, 100);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TakeCoin(int value)
+    {
+        coins += value;
+        BL_Animate = true;
+        BL_Increase = true;
+        StartCoroutine(CoinAnimation());
+    }
+
+    IEnumerator CoinAnimation()
+    {
+        while (BL_Animate == true)
+        {
+            if(BL_Increase)
+                currentTime += Time.deltaTime;
+            else
+                currentTime -= Time.deltaTime;
+
+            timeinterval = currentTime / timeOfTravel; // we normalize our time 
+
+            if (timeinterval > 1)
+                BL_Increase = !BL_Increase;
+
+            if (timeinterval <= 0)
+            {
+                currentTime = 0;
+                BL_Animate = false;
+            }
+
+            myCoin.anchoredPosition = Vector3.Lerp(startPos, endPos, timeinterval);
+            yield return null;
+        }
+    }
+}
