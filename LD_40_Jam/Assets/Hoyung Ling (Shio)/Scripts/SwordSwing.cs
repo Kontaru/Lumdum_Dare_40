606728e618409c2b39a6c898cdfb51b57b7217d0
Kontaru <drwafflesxd@gmail.com>
2017-12-03 23:37:20 +0000

    Auto stash before merge of "master" and "origin/master"

--- LD_40_Jam/Assets/Hoyung Ling (Shio)/Scripts/SwordSwing.cs
+++ LD_40_Jam/Assets/Hoyung Ling (Shio)/Scripts/SwordSwing.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwordSwing : MonoBehaviour {
-	
+
+    public bool BL_BelongsToEnemy = false;
+
 	void OnTriggerEnter(Collider coll)
     {
-        if (coll.gameObject != null && coll.gameObject.tag != "Player")
+        if (coll.gameObject != null && coll.gameObject.tag != "Player" && !BL_BelongsToEnemy)
+        {
+            coll.gameObject.SendMessage("TakeDamage", 1, SendMessageOptions.DontRequireReceiver);
+            Destroy(gameObject);
+        }else if (coll.gameObject != null && BL_BelongsToEnemy)
         {
             coll.gameObject.SendMessage("TakeDamage", 1, SendMessageOptions.DontRequireReceiver);
             Destroy(gameObject);
