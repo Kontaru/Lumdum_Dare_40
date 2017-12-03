f014d83af6ce4308c22cb85f7180092892b7ae5b
Oliver Knight <oliknight95@gmail.com>
2017-12-03 16:58:37 +0000

    Oli: Level Changes

---
+++ LD_40_Jam/Assets/OK_Stuff/Scripts/OK_OpenClose.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OK_OpenClose : MonoBehaviour {
+
+    // Setting Gameobject variables to be used.
+    public GameObject mGO_Player;
+    public GameObject mGO_Target;
+
+    public bool bl_Enable;
+    
+    // Use this for initialization
+	void Start () {
+
+        mGO_Player = GameObject.FindGameObjectWithTag("Player");
+
+        if (bl_Enable == true)
+            mGO_Target.SetActive(false);
+
+
+	}
+
+    private void OnTriggerEnter(Collider cl_trigger)
+    {
+        if (cl_trigger.gameObject == mGO_Player)
+        {
+            Destroy(gameObject);
+            if (bl_Enable == true)
+            {
+                mGO_Target.SetActive(true);
+            }
+            else if (bl_Enable == false)
+            {
+                mGO_Target.SetActive(false);
+            }
+
+        }
+    }
+
+}
