70b11041a5f8a8426dfe10f434e1f9cda900de0a
Kontaru <drwafflesxd@gmail.com>
2017-12-04 16:25:08 +0000

    Hoyung: Player slash

---
+++ LD_40_Jam/Assets/Hoyung Ling (Shio)/Scripts/SwordLungeAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordLungeAnimation : MonoBehaviour {
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject != null && coll.gameObject.tag == "Player")
+        {
+            coll.gameObject.SendMessage("TakeDamage", 1, SendMessageOptions.DontRequireReceiver);
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        AudioManager.instance.Play("BossDamage");
+    }
+
+    void Update()
+    {
+        Destroy(gameObject, 0.2f);
+    }
+}
